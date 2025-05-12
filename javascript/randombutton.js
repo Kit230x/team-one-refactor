de22c44e3969c9765f8ef1b24dba63c7d2a1c7ba
Kelsi S <ksalaetsx@gmail.com>
2025-05-11 23:19:25 -0600

    fixing things

--- javascript/randombutton.js
+++ javascript/randombutton.js
@@ -18,11 +18,12 @@ document.addEventListener("DOMContentLoaded", function () {
             "/classic-games/rock-paper-scissors.html",
             "/classic-games/tic-tac-toe.html",
             "/retro-games/pong.html",
-            "/retro-games/space-invaders.html",
             "/retro-games/snake.html",
             "/strategy-games/2048.html",
             "/strategy-games/chess.html",
             "/strategy-games/minesweeper.html",
+            "/retro-games/tanks.html",
+            "/arcade-games/connect-four.html",
         ];
         const randomGame = gamePages[Math.floor(Math.random() * gamePages.length)];
         window.location.href = randomGame;
